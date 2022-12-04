21a6489900045df8411f660ec2414585acdea26a
Diane Leigh <leighd2008@gmail.com>
2022-12-04 15:07:07 -0500

    used preload.js to correct Uncaught Reference Error: require is not defined, now will save to file using dialog box

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const electron = require('electron')
-const { app, BrowserWindow } = electron
+const fs = require('fs')
+const { app, BrowserWindow, ipcMain, dialog } = electron
+const path = require("path");
+let win
 
 
 app.on('ready', () => {
-  let win = new BrowserWindow({})
+  win = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"), 
+    //   nodeIntegration: true,
+    //   contextIsolation: false,
+    }
+  })
   win.loadFile('index.html')
+})
+
+ipcMain.on('save', (event, text)=>{
+  //save the test to a file
+  dialog.showSaveDialog(win, {defaultPath: 'filename.txt'})
+  .then((res) => {
+    if(res.canceled === false) {
+      fs.writeFile(res.filePath, text, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!')
+      })
+    } else {
+      console.log('File save was canceled.')
+    }
+  }).catch((err) => console.log(err))
+  // fs.writeFile('samplefile.txt', text, (err) => {
+  //   if(err) console.log('there was an error', err)
+  //   console.log('file has been saved')
+  // })
 })
